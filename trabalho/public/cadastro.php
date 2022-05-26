a50ec705450e2853eaf1bec2207012a7f28ba0b1
ルーカスバルガス <lucas.vargas120405@gmail.com>
2022-05-26 11:35:46 -0300

    Add files via upload

---
+++ trabalho/public/cadastro.php
@@ -0,0 +1,29 @@
+<?php require_once "../src/views/header_cad.php"; ?>
+
+
+<div class="login-center">
+        <img class="imagemfoda" src="../public/assets/img/logo.png">
+        <form method="post" action="receba.php">
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12">
+                        <input type="text"  class="form-control" name="nome" placeholder="Digite seu nome" required />
+                        <br>
+                        <input type="email"  class="form-control" name="email" placeholder="Digite seu email" required />
+                        <br>
+                        <input type="text" class="form-control" name="fone" placeholder="Digite seu telefone" required />
+                        <br>
+                        <input type="password" class="form-control" name="pass" placeholder="Digite sua senha" required />
+                        <br><br>
+                    </div>
+                </div>
+                <div class="d-flex">
+                    <input type="submit" class="btn btn-dark col-lg-4 offset-lg-2 col-sm-12" value="Pronto" />
+                    &nbsp;
+                    <input type="reset" class="btn btn-secondary col-lg-4 col-sm-12" value="Limpar" />
+                    <br><br>
+                </div>
+            </div>
+    </div>
+
+<?php require_once "../src/views/footer.php"; ?>
